624e8a43db4049568bf3bb5d58c74f2d02067d8e
I-AM-ISHIZAKI <ALINAISHIZAKI@OUTLOOK.COM>
2022-10-10 15:24:25 -0400

    comment.js and plant.js updated

--- models/plant.js
+++ models/plant.js
@@ -7,21 +7,26 @@ const User = require('./user')
 // destructure the schema and model constructors from mongoose
 const { Schema, model } = mongoose
 
-const plantSchema = new Schema(
+// // importing the commentSchema
+// const commentSchema = require('./comment')
+
+const PlantSchema = new Schema(
 	{
-		title: { type: String, required: true },
-		body: { type: String, required: true },
-        amount: { type: Number, required: true },
-		ready: { type: Boolean, required: true },
+		name: { type: String, required: true },
+		sciName: { type: String, required: true },
+        sunniness: { type: String, required: true },
+		plantFrom: { type: Boolean, required: true },
+		description: { type: String, required: true },
 		owner: {
 			type: Schema.Types.ObjectID,
 			ref: 'User',
-		}
+		},
+		comments: [commentSchema]
 	},
 	{ timestamps: true }
 )
 
-const Plant = model('Plant', plantSchema)
+const Plant = model('Plant', PlantSchema)
 
 /////////////////////////////////
 // Export our Model
